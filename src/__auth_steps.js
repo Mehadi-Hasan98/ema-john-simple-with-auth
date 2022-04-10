88b61e6cb7786e115eef4f1d57e102cda309989a
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-04-11 00:35:04 +0600

    Login form added

---
+++ src/__auth_steps.js
@@ -0,0 +1,22 @@
+/*
+* 1. create a new project in console.google.com
+* 2. npm install firebase
+* 3. create firebase.init.js and import getAuth to export auth.
+* 4. Firebase settings > Authentication > Enable Email and Password auth
+* 5. 
+* 6.
+* 7.
+*/ 
+
+
+
+
+
+
+/**
+ * hosting steps 
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 3. firebase init
+ * 4. firebase deploy
+ * **/ 
